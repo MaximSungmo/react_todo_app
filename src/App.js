248d2d6dcacd8defde12ec3b6ed188484081c9a9
MaximSungmo <ksm5318@naver.com>
2020-09-20 00:18:56 +0900

    Add function
    - Update
    - Delete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Button, FormControl, Input, InputLabel} from '@material-ui/core';
 import './App.css';
 import Todo from "./Todo";
 import db from './firebase';
+import firebase from "firebase";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -10,15 +11,23 @@ function App() {
 
   // when the app load, we need to listen to the database and fetch new todos as they get added/removed
   useEffect(() => {
-    /// this code here... fires when the app.js loads
-    db.collection('todos').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().todo));
-    })
-  }, []);
+        /// this code here... fires when the app.js loads
+        db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+          setTodos(snapshot.docs.map(doc => ({
+            id: doc.id,
+            todo: doc.data().todo
+          })))
+        })
+      }
+      , []);
 
   const addTodo = (event) => {
     // this will fire off when we click the button
     event.preventDefault(); // I will stop refresh the page
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
     setTodos([...todos, input]);
     setInput(''); // clear up the input after hitting submit
   }
@@ -37,7 +46,7 @@ function App() {
         </Button>
         <ul>
           {todos.map(todo => (
-              <Todo text={todo}/>
+              <Todo todo={todo}/>
           ))}
         </ul>
       </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,15 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import './Todo.css';
-import {ListItemAvatar, List, ListItem, ListItemText} from "@material-ui/core";
+import {
+  ListItemAvatar,
+  List,
+  ListItem,
+  ListItemText,
+  Modal, Input
+} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import db from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function Todo(props) {
+  const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+
+  const handleOpen = () => {
+    setOpen(false);
+  };
+
+  const updateTodo = () => {
+    // update the todo with the new input text
+    db.collection('todos').doc(props.todo.id).set({
+      todo: input
+    }, {merge: true});
+    setOpen(false);
+  }
+
   return (
-      <List className="todo__list">
-        <ListItem>
-          <ListItemAvatar/>
-          <ListItemText primary={props.text} secondary="Todo"/>
-        </ListItem>
-      </List>
+      <>
+        <Modal open={open} onClose={handleOpen}>
+          <div className={classes.paper}>
+            <h1>Update Todo</h1>
+            <Input
+                placeholder={props.todo.todo}
+                value={input}
+                onChange={event => setInput(event.target.value)}
+            />
+            <Button onClick={e => updateTodo()}>Update todo</Button>
+          </div>
+        </Modal>
+        <List className="todo__list">
+          <ListItem>
+            <ListItemAvatar/>
+            <ListItemText id={props.todo.id} primary={props.todo.todo}
+                          secondary="Todo"/>
+          </ListItem>
+          <Button onClick={e => setOpen(true)}>edit</Button>
+          <DeleteForeverIcon onClick={event => {
+            db.collection('todos').doc(props.todo.id).delete();
+          }}/>
+        </List>
+      </>
   )
 }
 
